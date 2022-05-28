99defc286484872c973819eea8e8d45ef046fba1
97clinton <105053533+97Clinton@users.noreply.github.com>
2022-05-28 23:57:31 +0100

    Add files via upload
    My ZURI CLASS TASK

---
+++ main.py
@@ -0,0 +1,36 @@
+class Student:
+
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, track, score):
+        self.name = name
+        self.age = age
+        self.track = track
+        self.score = score
+
+
+
+    def change_name(self, name):
+        self.name = name
+
+
+    def change_age(self, age):
+        self.age = int(age)
+
+
+    def add_track(self, track):
+        self.track = track
+   
+
+    def get_score(self, score):
+        self.score = float(score)
+
+
+Bob = Student(name="Bob", age=26, track=["FE", "BE"], score=20.90)
+
+
+
+# Expected methods
+    Bob.change_name("Peter")
+    Bob.change_age(34)
+    Bob.add_track("UI/UX")
+    Bob.get_score(12.698)
